5a220f8eda0fcccae7da16a1e778852531c2794b
Stocy <tomdavid99@gmail.com>
2023-01-25 15:44:08 +0100

    Update PlayerController.cs

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : NetworkBehaviour
