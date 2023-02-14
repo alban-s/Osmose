44c3f0168a3ef101a9b49406fbdb46ee7cb679c8
Etienne Rochette-Berlon <106489461+Estriam@users.noreply.github.com>
2023-02-14 16:38:33 +0100

    fix bug si mort -> score = USHORT_MAX

--- Assets/Scripts/Shared/Health.cs
+++ Assets/Scripts/Shared/Health.cs
@@ -125,12 +125,12 @@ namespace Osmose.Gameplay
             GameObject attackHitbox = transform.Find("attackCollider").gameObject;
             if (attackHitbox.GetComponent<Associate>().GetAssociatedPlayer() != null && !associated)
             {
-                CurrentPoints -= (ushort)(damage / 2);
+                CurrentPoints = (damage/2>=CurrentPoints)? 0 : (ushort)(CurrentPoints - damage / 2);
                 attackHitbox.GetComponent<Associate>().GetAssociatedPlayer().GetComponent<Health>().IncreasePoints((ushort)(damage / 2), damageSource, true);
             }
             else
             {
-                CurrentPoints -= damage;
+                CurrentPoints = (damage>=CurrentPoints)? 0 : (ushort) (CurrentPoints - damage);
             }
             update_score_clients();
             //CurrentPoints -= damage;
