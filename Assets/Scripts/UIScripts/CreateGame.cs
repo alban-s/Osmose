54f70e28250da2c8bad441057d89398c951ba76a
lumoon <93905358+lumoon@users.noreply.github.com>
2023-02-15 19:48:15 +0100

    minutes et correction script/UI NewGamePanel
    c'est normal de pas appeler les fonctions initNbPlayer() ect car la liaison de fait direct par unity des qu'il y a un changement dans les input donc arretez de modif mon code pour de la merde merci

--- Assets/Scripts/UIScripts/CreateGame.cs
+++ Assets/Scripts/UIScripts/CreateGame.cs
@@ -12,14 +12,14 @@ public class CreateGame : MonoBehaviour
 
     public GameObject thisWindow;
     public GameObject persoSelectionWindow;
-    public InputField playerNb;
+/*    public InputField playerNb;
     public InputField maxTeamPoints;
-    public InputField gameDuration;
+    public InputField gameDuration;*/
 
     private GameObject gameManager;
     private ushort nbPlayers;
     private ushort nbPtsPerTeam;
-    private float gameTime;
+    private ushort gameTime;
 
     private void Awake()
     {
@@ -28,17 +28,17 @@ public class CreateGame : MonoBehaviour
 
     public void InitNbPlayers(string value)
     {
-        nbPlayers = ushort.Parse(value); // Conversion pas propre a reprendre
+        nbPlayers = ushort.Parse(value);
     }
 
     public void InitPtsPerTeam(string value)
     {
-        nbPtsPerTeam = ushort.Parse(value); // Conversion pas propre a reprendre
+        nbPtsPerTeam = ushort.Parse(value);
     }
 
-    public void InitGameTime(string value)
+    public void InitGameTime(string minutes)
     {
-        gameTime = float.Parse(value);// Conversion pas propre a reprendre
+        gameTime = ushort.Parse(minutes);
     }
 
 
@@ -46,13 +46,10 @@ public class CreateGame : MonoBehaviour
     {
         print("serveurToLink\n");
         print("nb Joueurs =" + nbPlayers.ToString() + ", Points par �quipes =" +
-            nbPtsPerTeam.ToString() + " et gameTime =" + gameTime.ToString(".##"));
+            nbPtsPerTeam.ToString() + " et gameTime =" + gameTime.ToString());
            
         manager.StartHost();
 
-        InitNbPlayers(playerNb.text);
-        InitPtsPerTeam(maxTeamPoints.text);
-        InitGameTime(gameDuration.text);
 
         gameManager.GetComponent<GameManager>().SetMaxNbOfPlayer(nbPlayers);
         gameManager.GetComponent<Score>().SetTeamMaxScore(nbPtsPerTeam);
