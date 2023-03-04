c6b86e8d92280dba104bab55c69be133db6ca000
Alban Saint-sorny <a.saintsorny@orange.fr>
2023-03-04 15:25:31 +0100

    lancement animation au début

--- Assets/Scripts/UIScripts/MainMenuGestion.cs
+++ Assets/Scripts/UIScripts/MainMenuGestion.cs
@@ -19,21 +19,21 @@ public class MainMenuGestion : MonoBehaviour
         Screen.fullScreen = false;
         timeBegin = Time.time;
 
-        mainWindow.SetActive(true);
+        mainWindow.SetActive(false);
         settingWindow.SetActive(false);
         newGameWindow.SetActive(false);
         joinGameWindow.SetActive(false);
         initPersoWindow.SetActive(false);
-        cinematicWindow.SetActive(false);
+        cinematicWindow.SetActive(true);
 }
 
     void Update()
     {
-        /*if(Time.time - timeBegin >= 32.5)
+        if (Time.time - timeBegin >= 32.5)
         {
             cinematicWindow.SetActive(false);
             mainWindow.SetActive(true);
-        }*/
+        }
     }
 
     public void StartNewGame()
